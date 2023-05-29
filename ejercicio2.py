d6475be7d55893aba3ea874ebcf3deb0ad785e08
PeterRobotica08 <114919881+PeterRobotica08@users.noreply.github.com>
2023-05-29 13:33:44 +0200

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,14 @@
+x = 3
+y = 3.3
+
+print("el tipo de dato de x es ", type(x))
+
+x = str(x)
+
+print("el tipo de dato de x es ", type(x))
+
+print("el tipo de dato de x es ", type(y))
+
+y = str(y)
+
+print("el tipo de dato de x es ", type(y))
